fe8376712a4c8b7725c872d0043ff0c98db5c49e
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-07-05 22:45:50 +0530

    init

---
+++ censor.py
@@ -0,0 +1,15 @@
+import re
+import subprocess
+
+def censor(input_file, output_file):
+    phone_pattern = r'\(\d{3}\)\s*\d{3}-\d{4}'
+    with open(input_file, 'r') as file:
+        content = file.read()
+    content = re.sub(phone_pattern, 'CENSORED', content)
+    with open(output_file, 'w') as file:
+        file.write(content)
+
+
+censor("resume.tex", "resume_pub.tex")
+subprocess.run(["pdflatex", "resume_pub.tex"])
+
