8eee7b12801f73d5dc2119ebf18d433932c943a2
Sahil Sinha <dev.sahilsinha@gmail.com>
2024-07-31 17:49:54 +0530

    Updated

--- censor.py
+++ censor.py
@@ -5,12 +5,12 @@ def censor(input_file, output_file):
     phone_pattern = r'\(\d{3}\)\s*\d{3}-\d{4}'
     with open(input_file, 'r') as file:
         content = file.read()
-    content = re.sub(phone_pattern, 'CENSORED', content)
+    content = re.sub(phone_pattern, '', content)
     with open(output_file, 'w') as file:
         file.write(content)
 
 
-censor("resume.tex", "resume_pub.tex")
+censor("./resume_original/Sahil_Sinha_Resume.tex", "resume_pub.tex")
 subprocess.run(["pdflatex", "resume_pub.tex"])
 subprocess.run(["rm", "-f", "resume_pub.aux",  "resume_pub.log",  "resume_pub.out", "resume_pub.tex"])
 subprocess.run(["mv", "resume_pub.pdf", "pub/"])
